7fd2787009e944a596d91effe0c9177189a03fea
Davyd Holovii <mirage.present@gmail.com>
2021-11-08 23:35:28 +0200

    The simplest test case

---
+++ tests/GameOfLifeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use MiragePresent\GoL\Game;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GameOfLifeTest
+ *
+ * @author Davyd Holovii <mirage.present@gmail.com>
+ * @since  08.11.2021
+ */
+class GameOfLifeTest extends TestCase
+{
+    public function test_default_case_with_borders()
+    {
+        $input = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0],
+        ];
+
+        $game = new Game($input);
+        $fistGeneration = $game->nextGeneration();
+
+        $this->assertEquals(
+            [
+                [0, 0, 0, 0, 0],
+                [0, 1, 0, 1, 0],
+                [0, 0, 1, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0],
+            ],
+            $fistGeneration
+        );
+    }
+}
